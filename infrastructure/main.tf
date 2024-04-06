6f95fc3616ea302acfc77ce6af1f1c406f0605db
akilblanchard <akiltb@hotmail.com>
2024-04-05 22:46:13 -0500

    Added third subnet to imporve availability of the infrastructure

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   # Pass variables to the VPC module
   cidr_block         = "10.0.0.0/16"
-  public_cidr_block  = ["10.0.1.0/24","10.0.2.0/24"]
-  private_cidr_block = ["10.0.10.0/24","10.0.11.0/24"]
-  availability_zone  = ["us-east-1a","us-east-1b"]
+  public_cidr_block  = ["10.0.1.0/24","10.0.2.0/24", "10.0.3.0/24"]
+  private_cidr_block = ["10.0.10.0/24","10.0.11.0/24", "10.0.12.0/24"]
+  availability_zone  = ["us-east-1a","us-east-1b", "us-east-1c"]
 }
